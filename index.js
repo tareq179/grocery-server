473cd3af3530939d3ab3926b3bfecc9d179b9f37
tareq179 <abdulkarimctg373@gmail.com>
2021-07-07 20:59:06 +0600

    add delete update API

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 const port = process.env.PORT || 5500;
 
 app.get("/", (req, res) => {
-  res.send("Hello the app is working working!");
+  res.send("Hello the FootSto app is working working!");
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g5ktv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -27,20 +27,63 @@ client.connect((err) => {
     const ProductCollection = client.db("footsto").collection("product");
     const OrderCollection = client.db("footsto").collection("order");
     const ReviewCollection = client.db("footsto").collection("review");
+    const CartCollection = client.db("footsto").collection("cart");
 
     app.get("/isAdmin",(req,res)=>{
         AdminCollection.find({email: req.query.email}).toArray((err,docs)=>{
             res.send(!!docs.length);
         })
     })
+    
     app.get("/product",(req,res)=>{
         ProductCollection.find().toArray((err,docs)=>{
             res.send(docs);
         })
     })
 
+    app.get("/cart",(req,res)=>{
+        CartCollection.find({email: req.query.email}).toArray((err,docs)=>{
+            res.send(docs);
+        })
+    })
+
+    app.get("/order",(res,req)=>{
+        AdminCollection.find({email: req.query.email}).toArray((err,docs)=>{
+            if(docs.length){
+                OrderCollection.find().toArray((err,docs)=>{
+                    res.send(docs);
+                })
+            }
+            else{
+                OrderCollection.find({email: req.query.email}).toArray((err,docs)=>{
+                    res.send(docs);
+                })
+            }
+        })
+    })
+
+
+
     app.post("/addAdmin",(req,res)=>{
         AdminCollection.insertOne(req.body).then((result)=>{
+            res.send(!!result.insertedCount > 0);
+        });
+    });
+
+    app.post("/addOrder",(req,res)=>{
+        OrderCollection.insertOne(req.body).then((result)=>{
+            res.send(!!result.insertedCount > 0);
+        });
+    });
+
+    app.post("/addReview",(req,res)=>{
+        ReviewCollection.insertOne(req.body).then((result)=>{
+            res.send(!!result.insertedCount > 0);
+        });
+    });
+
+    app.post("/addToCart",(req,res)=>{
+        CartCollection.insertOne(req.body).then((result)=>{
             res.send(!!result.insertedCount > 0);
         });
     });
@@ -50,6 +93,56 @@ client.connect((err) => {
             res.send(!!result.insertedCount > 0);
         });
     });
+
+    app.delete("/deleteProduct/:id",(req,res)=>{
+        ProductCollection.deleteOne({_id: ObjectId(req.params.id)}).then((result)=>{
+            res.send(!!result.deletedCount)
+        });
+    });
+
+    app.delete("/deleteCart/:id",(req,res)=>{
+        CartCollection.deleteOne({_id: ObjectId(req.params.id)}).then((result)=>{
+            res.send(!!result.deletedCount)
+        });
+    });
+
+    app.delete("/deleteReview/:id",(req,res)=>{
+        ReviewCollection.deleteOne({_id: ObjectId(req.params.id)}).then((result)=>{
+            res.send(!!result.deletedCount)
+        });
+    });
+
+    app.patch("/updateOrderStatus",(req,res)=>{
+        const {id, status} = req.body;
+        OrderCollection.findOneAndUpdate(
+            {_id: ObjectId(id)},
+           { $set: {status}}
+            )
+            .then((result)=>{
+                res.send(result.lastErrorObject.updatedExisting)
+            });
+    });
+    
+    app.patch("/updateProduct/:id",(req,res)=>{
+        ProductCollection.updateOne(
+            {_id: ObjectId(req.params.id)},
+            {$set:req.body},
+        );
+        .then((result)=>{
+            res.send(!!result.modifiedCount);
+        });
+    });
+    
+    app.patch("/updateReview/:id",(req,res)=>{
+        ReviewCollection.updateOne(
+            {_id: ObjectId(req.params.id)},
+            {$set:req.body},
+        );
+        .then((result)=>{
+            res.send(!!result.modifiedCount);
+        });
+    });
+
 });
 
 app.listen(port, () => {
